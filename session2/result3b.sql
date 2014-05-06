f0bf188927583beb33c23f92e0970a9a1ebb793d
Victor Lap <victorlap@outlook.com>
2014-05-06 16:33:08 +0200

    Remove bullshit in 3b
    Remove unnecessary extra queries

--- session2/result3b.sql
+++ session2/result3b.sql
@@ -1,10 +1,8 @@
-SELECT p.pid FROM Person p
-WHERE EXISTS (
-	SELECT * FROM Acts a
+
+	SELECT a.pid FROM Acts a
 	WHERE a.pid = p.pid
 	AND EXISTS (
 		SELECT * FROM Movie m
 		WHERE m.mid = a.mid
 		AND m.name = 'Back to the Future'
-		)
-	);
+		);
