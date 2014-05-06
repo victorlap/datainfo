138576a244d10925d9f807960341ff612eedd0b9
Victor Lap <victorlap@outlook.com>
2014-05-06 16:13:37 +0200

    Added Exercise 1

---
+++ session2/exercise1.sql
@@ -0,0 +1,5 @@
+CREATE TABLE Problem1 (
+	task CHAR(1) PRIMARY KEY,
+	result BOOLEAN,
+	motivation VARCHAR(200)
+);
---
+++ session2/result1.sql
@@ -0,0 +1,11 @@
+
+INSERT INTO Problem1 (task, result, motivation) VALUES ("a", false, "Er kunnen meerdere auto's zijn met dezelfde capaciteit" );
+INSERT INTO Problem1 (task, result, motivation) VALUES ("b", true,  "De capaciteit van een voertuig is bindend" );
+INSERT INTO Problem1 (task, result, motivation) VALUES ("c", true,  "Door c) want een pakje wordt op hooguit een factuur vermeld" );
+INSERT INTO Problem1 (task, result, motivation) VALUES ("d", true,  "Volgt uit 7)" );
+INSERT INTO Problem1 (task, result, motivation) VALUES ("e", true,  "Blijkt uit 5) en 6)" );
+INSERT INTO Problem1 (task, result, motivation) VALUES ("f", false, "Een bezorger kan voor meerdere voertuigen gerechtigd zijn" );
+INSERT INTO Problem1 (task, result, motivation) VALUES ("g", false, "Een bezorger kan voor meerdere voertuigen gerechtigd zijn" );
+-- INSERT INTO Problem1 (task, result, motivation) VALUES (`h`, true, "" );
+-- INSERT INTO Problem1 (task, result, motivation) VALUES (`i`, true, "" );
+-- INSERT INTO Problem1 (task, result, motivation) VALUES (`j`, true, "" );
